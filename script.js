2fdfd1438f2ae325dfaaa5c752584c461792d801
kunal0510-sys <kunalr2205@gmail.com>
2025-05-13 07:54:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,47 +1,37 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['CM',900],
-	  2:['D',500],
-      3:['CD',400],
-	  4:['C', 100], 
-      5:['XC',90],
-	  6:['L',50], 
-      7:['XL',40], 
-      8:['X',10],
-	  9:['IX',9],
-	  10:['V',5], 
-	  11:['IV',4],
-	  12:['I',1],
-	};
-	let n=798;
-	let ans=""
-	while(n!=0)
-	{
-		for (let i in romanMap)
-			{
-				let value=romanMap[i][0];
-				let num=romanmap[i][1];
-				if(num<=n)
-				{
-					ans=ans+value;
-					n=n-num;
-					break;
-				}
-			}
-	}
-	retuen ans;
-    };
+  const romanSymbols = [
+    ['M', 1000],
+    ['CM', 900],
+    ['D', 500],
+    ['CD', 400],
+    ['C', 100],
+    ['XC', 90],
+    ['L', 50],
+    ['XL', 40],
+    ['X', 10],
+    ['IX', 9],
+    ['V', 5],
+    ['IV', 4],
+    ['I', 1]
+  ];
 
-  //your code here
+  let result = '';
 
+  for (let i = 0; i < romanSymbols.length; i++) {
+    const [symbol, value] = romanSymbols[i];
+
+    while (num >= value) {
+      result += symbol;
+      num -= value;
+    }
+  }
+
+  return result;
 }
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
-
-
-
+// Test cases
+console.log(convertToRoman(14));   // Output: XIV
+console.log(convertToRoman(798));  // Output: DCCXCVIII
 
 // do not edit below this line
-module.exports = convertToRoman
+module.exports = convertToRoman;
